45ff8935964ab2b3e27b0152de8cb6f5eddb5e0a
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-07 20:42:27 +0000

    Item8039: certified for foswiki/compat
    git-svn-id: http://svn.foswiki.org/trunk/NewUserPlugin@1824 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/NewUserPlugin.pm
+++ lib/TWiki/Plugins/NewUserPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2006-2008 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2006-2009 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -12,7 +12,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-# For licensing info read LICENSE file in the TWiki root.
+# For licensing info read LICENSE file in the Foswiki root.
 
 ###############################################################################
 package TWiki::Plugins::NewUserPlugin;
@@ -22,18 +22,16 @@ use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $NO_PREFS_IN_TOPIC $done);
 
 use constant DEBUG => 0; # toggle me
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
-# status of the plugin. It is used by the build automation tools, so
-# you should leave it alone.
 $VERSION = '$Rev$';
-$RELEASE = 'v1.05';
+$RELEASE = 'v1.10';
 $SHORTDESCRIPTION = 'Create a user topic if it does not exist yet';
 $NO_PREFS_IN_TOPIC = 1;
 
 ###############################################################################
 sub writeDebug {
-  #print STDERR 'NewUserPlugin - '.$_[0]."\n" if DEBUG;
-  TWiki::Func::writeDebug("NewUserPlugin - $_[0]") if DEBUG;
+  return unless DEBUG;
+  print STDERR 'NewUserPlugin - '.$_[0]."\n";
+  #TWiki::Func::writeDebug("NewUserPlugin - $_[0]");
 }
 
 ###############################################################################
@@ -51,48 +49,45 @@ sub initPlugin {
 }
 
 ###############################################################################
-# unfortunately we can't use the initializeUserHandler as TWiki is not
+# unfortunately we can't use the initializeUserHandler as the engine is not
 # fully initialized then. even the beforeCommonTagsHandler get's called in
-# a half-init state in the middle of TWiki::new. so we have to wait for
-# the TWiki object to be fully initialized, i.e. its i18n subsystem
+# a half-init state in the middle of the main constructor. so we have to wait for
+# the main object to be fully initialized, i.e. its i18n subsystem
 sub beforeCommonTagsHandler {
   return if !defined($TWiki::Plugins::SESSION->{i18n}) || $done;
   $done = 1;
 
-  writeDebug("called beforeCommonTagsHandler") if DEBUG;
+  #writeDebug("called beforeCommonTagsHandler");
 
   my $wikiUserName = TWiki::Func::getWikiUserName();
   my $mainWeb = TWiki::Func::getMainWebname();
 
   return if TWiki::Func::topicExists($mainWeb, $wikiUserName);
 
-  writeDebug("NO home topic found for $wikiUserName") if DEBUG;
+  writeDebug("NO home topic found for $wikiUserName");
   createUserTopic($wikiUserName)
 }
 
 ###############################################################################
-# decodes escape codes and expands VARs
 sub expandVariables {
-  my $text     = $_[0];
-  my $wikiName = $_[1];
-  my $mainWeb  = $_[2];
+  my ($text, $topic, $web) = @_;
 
-  # remove surrounding quotation marks
-  if ( $text =~ m/^\"(.*)\"$/ ) {
-    $text = $1;
-  }
+  return '' unless $text;
 
-  # decode $dollar and $percnt
-  $text =~ s/\$dollar/\$/g;
-  $text =~ s/\$percnt/%/g;
+  $text =~ s/^\"(.*)\"$/$1/go;
 
-  # expand vars
-  $text = TWiki::Func::expandCommonVariables($text, $wikiName, $mainWeb, undef);
+  my $found = 0;
+  my $mixedAlphaNum = TWiki::Func::getRegularExpression('mixedAlphaNum');
+
+  $found = 1 if $text =~ s/\$percnt/\%/go;
+  $found = 1 if $text =~ s/\$nop//go;
+  $found = 1 if $text =~ s/\$n([^$mixedAlphaNum]|$)/\n$1/go;
+  $found = 1 if $text =~ s/\$dollar/\$/go;
+
+  $text = TWiki::Func::expandCommonVariables($text, $topic, $web) if $found;
 
   return $text;
 }
-
-
 
 ###############################################################################
 # creates a user topic for the given wikiUserName
@@ -102,14 +97,14 @@ sub createUserTopic {
   my $twikiWeb = TWiki::Func::getTwikiWebname();
   my $mainWeb = TWiki::Func::getMainWebname();
   my $newUserTemplate =
-    TWiki::Func::getPreferencesValue('NEWUSERTEMPLATE') || 'NewLdapUserTemplate';
+    TWiki::Func::getPreferencesValue('NEWUSERTEMPLATE') || 'NewUserTemplate';
   my $tmplTopic;
   my $tmplWeb;
 
   # search the NEWUSERTEMPLATE
   $newUserTemplate =~ s/^\s+//go;
   $newUserTemplate =~ s/\s+$//go;
-  $newUserTemplate =~ s/\%SYSTEMWEB\%/$twikiWeb/g;
+  $newUserTemplate =~ s/\%TWIKIWEB\%/$twikiWeb/g;
   $newUserTemplate =~ s/\%SYSTEMWEB\%/$twikiWeb/g;
   $newUserTemplate =~ s/\%MAINWEB\%/$mainWeb/g;
 
@@ -129,7 +124,7 @@ sub createUserTopic {
     }
   }
 
-  writeDebug("newusertemplate = $tmplWeb.$tmplTopic") if DEBUG;
+  writeDebug("newusertemplate = $tmplWeb.$tmplTopic");
 
   # read the template
   my $text = TWiki::Func::readTopicText($tmplWeb, $tmplTopic);
@@ -141,25 +136,28 @@ sub createUserTopic {
   # insert data
   my $wikiName = TWiki::Func::getWikiName();
   my $loginName = TWiki::Func::wikiToUserName($wikiName);
-  $text =~ s/\$nop//go;
+  $text =~ s/\$nop//go; 
   $text =~ s/\%WIKINAME\%/$wikiName/go;
   $text =~ s/\%USERNAME\%/$loginName/go;
   $text =~ s/\%WIKIUSERNAME\%/$wikiUserName/go;
 
-  writeDebug("saving new home topic $mainWeb.$wikiName") if DEBUG;
+  writeDebug("saving new home topic $mainWeb.$wikiName");
   my $errorMsg = TWiki::Func::saveTopicText($mainWeb, $wikiName, $text);
-
   if ($errorMsg) {
     writeWarning("error during save of $tmplWeb.$tmplTopic: $errorMsg");
-  } elsif ( $text =~ m/\%EXPAND\{/ ) {
+    return;
+  } 
 
-    # expanding VARs in a second phase, after the topic file was created (to get correct $meta objects)
-    $text =~ s/\%EXPAND\{(.*?)\}\%/&expandVariables($1, $wikiName, $mainWeb)/ge;
+  # expanding VARs in a second phase, after the topic file was created (to get correct $meta objects)
+  my $found = 0;
+  $found = 1 if $text =~ s/\%EXPAND\{(.*?)\}\%/&expandVariables($1, $wikiName, $mainWeb)/ge;
+  $found = 1 if $text =~ s/\%STARTEXPAND\%(.*?)\%STOPEXPAND\%/TWiki::Func::expandCommonVariables($1, $wikiName, $mainWeb)/ges;
 
-    writeDebug("expanding vars in new home topic $mainWeb.$wikiName") if DEBUG;
-    my $errorExpandMsg = TWiki::Func::saveTopicText($mainWeb, $wikiName, $text);
-    if ($errorExpandMsg) {
-      writeWarning("error during save of var expanded version of $mainWeb.$wikiName: $errorExpandMsg");
+  if ($found) {
+    writeDebug("expanding vars in new home topic $mainWeb.$wikiName");
+    $errorMsg = TWiki::Func::saveTopicText($mainWeb, $wikiName, $text);
+    if ($errorMsg) {
+      writeWarning("error during save of var expanded version of $mainWeb.$wikiName: $errorMsg");
     }
   }
 }
--- lib/TWiki/Plugins/NewUserPlugin/build.pl
+++ lib/TWiki/Plugins/NewUserPlugin/build.pl
@@ -1,24 +1,11 @@
-
 #!/usr/bin/perl -w
 BEGIN {
     unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build('NewUserPlugin');
-
-# Set the details of the repository for uploads.
-# This can be any web on any accessible TWiki installation.
-
-# name of web to upload to
-$build->{UPLOADTARGETWEB} = 'Extensions';
-# Full URL of pub directory
-$build->{UPLOADTARGETPUB} = 'http://foswiki.org/pub';
-# Full URL of bin directory
-$build->{UPLOADTARGETSCRIPT} = 'http://foswiki.org/bin';
-# Script extension
-$build->{UPLOADTARGETSUFFIX} = '';
+$build = new Foswiki::Contrib::Build('NewUserPlugin');
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
